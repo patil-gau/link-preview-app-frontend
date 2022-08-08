f0343d64f449a93babfb3d35ef56c31765ef3370
patil-gau <patilgautam728@gmail.com>
2022-08-08 13:27:15 +0530

    heroku url configured

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const getMetaData = (url)=>{
     const data = {"url":url};
     console.log(data);
-    axios.post("http://localhost:8000/meta-data",data).then((res)=>{
+    axios.post("https://just-preview.herokuapp.com",data).then((res)=>{
        if(res.data.result.status==1){
         setmetaData(res.data.result.data)
         setshowPreview(true);
